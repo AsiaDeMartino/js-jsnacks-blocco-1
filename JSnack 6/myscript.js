af23f60db22d70d6f6d5bad0ad6d06b9682fb368
asia_demartino@libero.it <asia_demartino@libero.it>
2022-02-13 20:22:26 +0100

    JSnack 6

---
+++ JSnack 6/myscript.js
@@ -0,0 +1,18 @@
+// Stampa il cubo dei primi N numeri, dove N è un numero indicato dall’utente
+
+let numeroNumeri = parseFloat(prompt("inserisci fino a che numero vuoi il cubo"));
+while (isNaN(numeroNumeri)) {
+    alert("Devi inserire un numero");
+    numeroNumeri = parseFloat(prompt("inserisci fino a che numero vuoi il cubo"));
+}
+
+const numeriCubo = [];
+
+for ( i = 0; i < numeroNumeri; i++ ) {
+    let cubo = Math.pow( i+1, 3);
+    numeriCubo.push(cubo);
+}
+
+console.log(numeriCubo)
+alert(`il cubo dei tuoi numeri è ${numeriCubo}`);
+
